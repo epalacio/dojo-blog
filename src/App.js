49c3c47b9c2863c69251154de47312dd1a7b00c3
Esteban Palacio <estipalacio@gmail.com>
2021-01-07 14:46:20 -0500

    Added example dynamic values to undersand how React processes js, understanding the difference between expressions and statements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = 'https:www.google.com'
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked {likes} times</p>
+        <a target='_blank' rel="noreferrer" href={link}>Google site</a>
       </div>
     </div>
   );
