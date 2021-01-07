e833b3946f1d4ed7d79394c352905f81d0dea8ed
Esteban Palacio <estipalacio@gmail.com>
2021-01-07 16:01:54 -0500

    Added to clickable buttons to understand reacting via React in components. Passing params through functions needs to be done via an anonymous function

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello!', e);
+    };
+
+    const handleClickAgain = (name) => {
+       console.log(`Hello ${name}`); 
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClickAgain('Mario')}>Click me again</button>
         </div>
      );
 }
