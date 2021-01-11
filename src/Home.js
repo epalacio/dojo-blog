21e399757fa76e118884417cf6bc3b4ba33f71c3
Esteban Palacio <estipalacio@gmail.com>
2021-01-11 10:30:45 -0500

    Created error handling function by checking the 'ok' of the result, then throwing an error and using conditional && to display error message if this appears

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,30 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
+        fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                };
                 return res.json()
             })
             .then(data => {
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
+            })
+            .catch(e => {
+                setIsPending(false);
+                setError(e.message);
             });
-        }, 1000);
     }, []);
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
             {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" />}
